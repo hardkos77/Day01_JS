120fc546c71fba1f0ec72eb6f6fa4ab68d950051
hardkos77 <62692670+hardkos77@users.noreply.github.com>
2021-11-30 10:25:03 +0100

    ajout exo00

---
+++ Ex00/program.js
@@ -0,0 +1,14 @@
+function tabTriCarre(array){
+    for(let i = 0; i < array.length; i++) {
+        array[i] = Math.pow(array[i], 2);
+    }
+    array.sort(function(a, b) {
+        return a - b;
+    });
+    return array;
+}
+console.log(tabTriCarre(array));
+
+// Merci de ne pas effacer la ligne en dessous.
+
+exports.tabTriCarre =  tabTriCarre;
